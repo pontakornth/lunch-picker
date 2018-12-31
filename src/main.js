7c19cabb6711d3cf87505e26d3a5626b78efe4f4
pontakornth <pontakorn61@gmail.com>
2018-12-31 08:46:54 +0700

    add PWA support

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import './registerServiceWorker'
 Vue.config.productionTip = false
 Vue.use(Vuetify)
 new Vue({
